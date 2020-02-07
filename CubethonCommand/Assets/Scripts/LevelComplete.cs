dbc2ff8d3836e46ee16fc2ea8f013bd96c66b603
Devin Hicks <devinhicks.az@gmail.com>
2020-02-07 13:47:35 -0700

    fixing the develpoment I accidentally broke

--- CubethonCommand/Assets/Scripts/LevelComplete.cs
+++ CubethonCommand/Assets/Scripts/LevelComplete.cs
@@ -7,6 +7,8 @@ public class LevelComplete : MonoBehaviour
     public Canvas ReplayUI;
     public Rigidbody player;
 
+    public GameManager gameManager;
+
     public void Start()
     {
         player = player.GetComponent<Rigidbody>();
@@ -17,6 +19,8 @@ public class LevelComplete : MonoBehaviour
         {
             //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            gameManager.activateReplay();
+
         }
         else
         {
